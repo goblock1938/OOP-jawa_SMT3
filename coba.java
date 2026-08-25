a02d3144336e78ce45361a1a5570f0346d721a17
goblock1938 <goblock1938@gmail.com>
2026-08-25 15:05:21 +0700

    added trial version

--- coba.java
+++ coba.java
@@ -1,5 +1,10 @@
-public class coba {
+public class Main {
     public static void main(String[] args) {
         System.out.println("Halo, Java!");
+        byte inib = 100;
+        short inis = 1000;
+        int inii = 100000;
+        long inil = 10000000;
+        long inil2 = 10000000L;
     }
 }
