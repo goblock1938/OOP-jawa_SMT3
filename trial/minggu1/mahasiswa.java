9586e6186d9576b7286fbf465c872fbac914db42
goblock1938 <goblock1938@gmail.com>
2026-09-08 10:41:30 +0700

    cleaning struktural folder minggu 1

---
+++ trial/minggu1/mahasiswa.java
@@ -0,0 +1,9 @@
+package trial.minggu1;
+
+public class mahasiswa {
+  public static void main(String[] args) {
+    Mahasiswa mhs = new Mahasiswa(12345, "Jono");
+    System.out.println("NRP : " + mhs.getNrp());
+    System.out.println("Nama : " + mhs.getnama());
+  }
+}
